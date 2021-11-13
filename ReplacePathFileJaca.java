ab7d38b74bd87a06b4e137a5850b644886369b72
khali123 <khalid.biq2017@gmail.com>
2021-11-13 18:33:02 +0100

    Move File and delee

---
+++ ReplacePathFileJaca.java
@@ -0,0 +1,28 @@
+package proje;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class ReplacePathFileJaca {
+	public static void main(String[] args) throws IOException {
+		
+		File source = new File("C:\\Users\\kml\\Desktop\\espagnol\\Recibí carga inicial.pdf");
+		Path path = Paths.get("C:\\Users\\kml\\Desktop\\espagnol\\generales\\");
+
+		Path dest = Files.createDirectories(path);
+
+		File deste = new File("C:\\Users\\kml\\Desktop\\espagnol\\generales\\Re.pdf");
+		boolean isMoved = source.renameTo(deste);
+
+		if (source.mkdirs()) {
+			System.out.println("Directory is created!");
+		} else {
+			System.out.println("Failed to create directory!");
+		}
+
+	}
+}
